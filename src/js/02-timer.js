3fa030dc2339ed856a7f53c1f51aa6d7ec68b38b
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-17 22:22:41 +0300

    1

---
+++ src/js/02-timer.js
@@ -0,0 +1,7 @@
+const refs = {
+  input: document.querySelector('#datetime-picker'),
+  btnStart: document.querySelector('[data-start]'),
+};
+console.log(refs.input);
+console.log(refs.btnStart);
+
