831b553b5e35106777d695bdc61a572258ddea40
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-17 22:28:10 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,7 +1,18 @@
+import flatpickr from 'flatpickr';
+import 'flatpickr/dist/flatpickr.min.css';
+
+const options = {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+  onClose(selectedDates) {
+    console.log(selectedDates[0]);
+  },
+};
+flatpickr('#datetime-picker', options);
+
 const refs = {
   input: document.querySelector('#datetime-picker'),
   btnStart: document.querySelector('[data-start]'),
 };
-console.log(refs.input);
-console.log(refs.btnStart);
-
