70764f3ed6497f700fd41cb39132a0015ae3937b
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-18 21:39:35 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -8,6 +8,15 @@ const options = {
   minuteIncrement: 1,
   onClose(selectedDates) {
     console.log(selectedDates[0]);
+
+    if (Date.now() > selectedDates[0].getTime()) {
+      window.alert('"Please choose a date in the future"');
+      refs.btnStart.disabled = true;
+      refs.btnStart.classList.add('disabled');
+    } else {
+      refs.btnStart.disabled = false;
+      refs.btnStart.classList.remove('disabled');
+    }
   },
 };
 flatpickr('#datetime-picker', options);
