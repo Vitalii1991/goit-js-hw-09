31751700137c576e360827ddc765989ef309e900
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-19 11:17:35 +0300

    1

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -1,12 +1,22 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
+const refs = {
+  btnStart: document.querySelector('[data-start]'),
+  daysEl: document.querySelector('[data-days]'),
+  hoursEl: document.querySelector('[data-hours]'),
+  minutesEl: document.querySelector('[data-minutes]'),
+  secondsEl: document.querySelector('[data-seconds]'),
+};
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
+    console.log(selectedDates[0]);
+
     if (Date.now() > selectedDates[0].getTime()) {
       window.alert('Please choose a date in the future');
 
@@ -19,19 +29,14 @@ const options = {
 
     setInterval(() => {
       const deltaTime = selectedDates[0].getTime() - Date.now();
-      const timeComponents = convertMs(deltaTime);
-      console.log(timeComponents);
+      const { days, hours, minutes, seconds } = convertMs(deltaTime);
+
+      updateValueSpan({ days, hours, minutes, seconds });
     }, 1000);
   },
 };
+
 flatpickr('#datetime-picker', options);
-
-const refs = {
-  input: document.querySelector('#datetime-picker'),
-  btnStart: document.querySelector('[data-start]'),
-};
-
-refs.btnStart.addEventListener('submit', () => {});
 
 function convertMs(ms) {
   const second = 1000;
@@ -39,14 +44,23 @@ function convertMs(ms) {
   const hour = minute * 60;
   const day = hour * 24;
 
-  const days = Math.floor(ms / day);
-  const hours = Math.floor((ms % day) / hour);
-  const minutes = Math.floor(((ms % day) % hour) / minute);
-  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+  const days = addLeadingZero(Math.floor(ms / day));
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
 
   return { days, hours, minutes, seconds };
 }
 
 function addLeadingZero(value) {
-  padStart(2, 0);
+  return String(value).padStart(2, 0);
+}
+
+function updateValueSpan({ days, hours, minutes, seconds }) {
+  refs.daysEl.textContent = days;
+  refs.hoursEl.textContent = hours;
+  refs.minutesEl.textContent = minutes;
+  refs.secondsEl.textContent = seconds;
 }
