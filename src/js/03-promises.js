f849735d6b68ba55dbcf9bec3a056117da8c26a3
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-21 07:45:37 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,17 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  btnCreate: document.querySelector('[type="submit"]'),
+};
+
+refs.form.addEventListener('input', e => {
+  console.log(e.currentTarget);
+  console.log(e.target, e.target.value);
+});
+
+refs.btnCreate.addEventListener('submit', () => {});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
