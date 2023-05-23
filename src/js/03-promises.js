9daf976cdb03b54580ddcbbb9925e94596e390b5
Vitalii1991 <ironwarrior1991@gmail.com>
2023-05-23 09:21:20 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -2,21 +2,46 @@ import Notiflix from 'notiflix';
 
 const refs = {
   form: document.querySelector('.form'),
+  inputDelay: document.querySelector('[name="delay"]'),
+  inputStep: document.querySelector('[name="step"]'),
+  inputAmount: document.querySelector('[name="amount"]'),
   btnCreate: document.querySelector('[type="submit"]'),
 };
 
-refs.form.addEventListener('input', e => {
-  console.log(e.currentTarget);
-  console.log(e.target, e.target.value);
-});
-
-refs.btnCreate.addEventListener('submit', () => {});
-
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+refs.form.addEventListener('input', () => {
+  console.log('Amount: ', refs.inputAmount.value);
+  console.log('Step: ', refs.inputStep.value);
+  console.log('Delay: ', refs.inputDelay.value);
+
+  let step = refs.inputStep.value;
+
+  if (refs.inputAmount) {
+    for (let i = 1; i <= refs.inputAmount.value; i++) {
+      createPromise(i, refs.inputDelay.value + step)
+        .then(({ position, delay }) => {
+          Notiflix.Notify.success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          Notiflix.Notify.failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  }
+});
